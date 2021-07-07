10994896b9bf05156923c1c2943665199bd02ffb
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-07 12:37:34 +0530

    393. "DECREASE" complete

--- src/reducer.js
+++ src/reducer.js
@@ -27,6 +27,23 @@ function reducer(state, action) {
 
   if (action.type === DECREASE) {
     console.log("u decreased");
+
+    let tempCart = [];
+
+    if(action.payload.amount===1){
+      console.log('reaches cart limit 1')
+      tempCart = state.cart.filter((cartItem) => cartItem.id !== action.payload.id)
+
+    }else{
+        tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+    }
+
+    return {...state,cart:tempCart}
   }
 
   if (action.type === REMOVE) {
