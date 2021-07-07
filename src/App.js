15d412a1ff928bbc0dbf88537c0197ce252947c6
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-07 08:40:18 +0530

    382. Refactor- removing old code to start working on the redux app.

--- src/App.js
+++ src/App.js
@@ -12,45 +12,26 @@ import reducer from './reducer'
 
 //default value:
 const initialStore = {
-  count: 0,
-  name: "smith"
+  card: cartItems,
+  total: 0,
+  amount:0
 };
 
 
   
 const store = createStore(reducer, initialStore);
-store.dispatch({ type: DECREASE }); //action passed
-store.dispatch({ type: INCREASE }); 
-store.dispatch({ type: INCREASE }); 
  
-
-console.log('latest state :',store.getState());
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar card={store.getState()} />
+      <Navbar/>
       <CartContainer cart={cartItems} />
     </main>
   );
 }
 
 export default App;
-
-/* 
-store = use for storing data like a state.
-reducer = must be the parameter of the createStore(), its a normal js function
-    1. the reducer take 2 arguments
-      a.state : old state/state before update
-      b.action : what happen/what update 
-      [must be an object, with the proprty type and value as "action type" in string]
-    2. return : updated state / old state. 
-    3. we must alawys return the old state from the reducer function, if there is
-no updated state to be share after checking the action.  
-    
-** so reducer is used for updating store,Since we can't directly update/add data in
-the store.
-
- */
+ 
--- src/reducer.js
+++ src/reducer.js
@@ -2,15 +2,7 @@ import {DECREASE,INCREASE} from './components/actions'
 
 //reducer:
 function reducer(state, action) {  
-    if (action.type === DECREASE) {
-      //state.count= state.count-1 [* we can't mutate directly like this]
-      return {...state, count: state.count - 1,name:"anna" };
-    }
-  
-    if (action.type === INCREASE) {
-      //state.count= state.count-1 [* we can't mutate directly like this]
-      return { ...state,count: state.count + 1 };
-    }
+ 
    
     return state;
   }
