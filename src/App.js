ef2fb79c0c3863fd9ddb2bb1c50ca3f26d6733b5
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-07 07:30:04 +0530

    376. First Action

--- src/App.js
+++ src/App.js
@@ -8,18 +8,26 @@ import cartItems from "./cart-items";
 
 import { createStore } from "redux";
 
+//default value:
+const initialStore = {
+  count: 10,
+};
+
 //reducer:
-function reducer(state,action) {
+function reducer(state, action) {
   console.log("hhi redux");
-  console.log("state :",state, 'action :',action)
+  console.log("state :", state, "action :", action);
+  if (action.type === "DECREASE") {
+    //state.count= state.count-1 [* we can't mutate directly like this]
+    return { count: state.count - 1 };
+  }
   return state;
 }
+  
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: "DECREASE" }); //action passed
 
-const initialStore ={
-  count:10
-}
-const store = createStore(reducer,initialStore);
-console.log(store.getState())
+console.log(store.getState());
 
 function App() {
   // cart setup
@@ -39,7 +47,8 @@ store = use for storing data like a state.
 reducer = must be the parameter of the createStore(), its a normal js function
     1. the reducer take 2 arguments
       a.state : old state/state before update
-      b.action : what happen/what update
+      b.action : what happen/what update 
+      [must be an object, with the proprty type and value as "action type" in string]
     2. return : updated state / old state. 
     3. we must alawys return the old state from the reducer function, if there is
 no updated state to be share after checking the action.  
