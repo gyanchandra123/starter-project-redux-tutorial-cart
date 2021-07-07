4f053476a3e9749ad2b5b966c5114d02f7c0cc51
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-07 10:56:52 +0530

    387. Switch Statement Syntax

--- src/reducer.js
+++ src/reducer.js
@@ -2,10 +2,18 @@ import { DECREASE, INCREASE, CLEAR_CART } from "./components/actions";
 
 //reducer:
 function reducer(state, action) {
-  if (action.type === CLEAR_CART) {
+   if (action.type === CLEAR_CART) {
     return { ...state, cart: [] };
   }
   return state; // never forget to return the modified state , otherwise we will get eror.
+
+  /* switch (action.type) {
+    case CLEAR_CART:
+      return { ...state, cart: [] };
+
+    default:
+      return state;
+  } */
 }
 
 export default reducer;
