7ee5e1ebf3cf2b93829a0b6c8ab264f9956382d3
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-07 07:05:04 +0530

    375. getState

--- src/App.js
+++ src/App.js
@@ -9,17 +9,24 @@ import cartItems from "./cart-items";
 import { createStore } from "redux";
 
 //reducer:
-function reducer() {
+function reducer(state,action) {
   console.log("hhi redux");
+  console.log("state :",state, 'action :',action)
+  return state;
 }
-const store = createStore(reducer);
+
+const initialStore ={
+  count:10
+}
+const store = createStore(reducer,initialStore);
+console.log(store.getState())
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar card={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
@@ -30,4 +37,14 @@ export default App;
 /* 
 store = use for storing data like a state.
 reducer = must be the parameter of the createStore(), its a normal js function
+    1. the reducer take 2 arguments
+      a.state : old state/state before update
+      b.action : what happen/what update
+    2. return : updated state / old state. 
+    3. we must alawys return the old state from the reducer function, if there is
+no updated state to be share after checking the action.  
+    
+** so reducer is used for updating store,Since we can't directly update/add data in
+the store.
+
  */
